1c7f56af2338c4902f13298dc83ee5f804ee8370
ronylitv <rostislavlitvinets@gmail.com>
2021-06-29 19:44:49 +0300

    'last_change'

--- Additional_Task/chernetka.py
+++ Additional_Task/chernetka.py
@@ -1,2 +1,19 @@
-new_list = ['mama', 'tato', 'bodya', '123', '123', '123']
-n = 0
+
+
+def real_len(text):
+    new_list = ['\n', '\f', '\r', '\t', '\v']
+    count = 0
+    for i in text:
+        if i in new_list:
+            count += 1
+    return len(text) - count
+
+text = 'Hello\nWorld\v'
+print(len(text))
+print(real_len(text))
+
+
+
+
+
+
