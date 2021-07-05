1d613276af6fb5710cbabb52a20ca4263512ea06
ronylitv <rostislavlitvinets@gmail.com>
2021-07-05 17:29:47 +0300

    'last_change'

--- module5/hw/9.py
+++ module5/hw/9.py
@@ -1,11 +1,24 @@
+# def formatted_numbers():
+#     num_list = []
+#     begin = '|{:^10}|{:^10}|{:^10}|'.format('decimal', 'hex', 'binary')
+#     num_list.append(begin)
+#     for i in range(0, 16):
+#         s = '|{0:^10d}|{0:^10x}|{0:^10b}|'.format(i)
+#         num_list.append(s)
+#     return num_list
+#
+# print(formatted_numbers())
+# for i in formatted_numbers():
+#     print(i)
+
+
 def formatted_numbers():
     num_list = []
-    begin = '|{:^10}|{:^10}|{:^10}|'.format('decimal', 'hex', 'binary')
-    num_list.append(begin)
+    num_list.append("|{:^10}|{:^10}|{:^10}".format("decimal", "hex", "binary"))
     for i in range(0, 16):
-        s = '|{0:^10d}|{0:^10x}|{0:^10b}|'.format(i)
+        s = "|{:<10d}|{:^10x}|{:>10b}".format(i,i,i)
         num_list.append(s)
-    return num_list
+    return (num_list)
 
-for i in formatted_numbers():
-    print(i)
+
+print(formatted_numbers())
