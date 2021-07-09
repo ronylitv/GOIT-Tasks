2cfda08cf8cac4a1f479daff44c0eaace8db81ae
ronylitv <rostislavlitvinets@gmail.com>
2021-07-09 20:08:14 +0300

    'last_change'

---
+++ module7/les2/useful/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='useful',
+      version='1',
+      description='Very useful code',
+      url='https://github.com/ronylitv/GOIT-Tasks/module7/les2/useful',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['useful'])
