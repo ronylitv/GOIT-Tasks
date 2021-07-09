4aa69581859cf19c40d3a9e54a2ecf9f2e24ed3b
ronylitv <rostislavlitvinets@gmail.com>
2021-07-09 19:34:43 +0300

    'last_change'

--- module6/hw/2/2.py
+++ module6/hw/2/2.py
@@ -1,12 +1,15 @@
-def write_employees_to_file(employee_list, path):
+def write_and_get_employees(employee_list, path):
     fh = open(path, 'w')
     for el in employee_list:
         for i in el:
             fh.write(i + '\n')
 
     fh.close()
+    fh = open(path, 'r')
+    lines = fh.readlines()
+    return lines
 
-write_employees_to_file([['Robert Stivenson,282', 'Alex Denver,30'],['Drake Mikelsson,19']], 'salary1.txt')
+print(write_and_get_employees([['Robert Stivenson,282', 'Alex Denver,30'],['Drake Mikelsson,19']], 'salary1.txt'))
 
 
 
